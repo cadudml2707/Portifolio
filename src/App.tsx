f5b65edd0432d34ff2b1e2dd233cb6e9a35b6745
carlos.lima <gbdev11@gbringel.com>
2024-12-12 13:43:40 -0400

    Criação da branch

--- src/App.tsx
+++ src/App.tsx
@@ -19,14 +19,14 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Contador {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edite <code>src/App.tsx</code> e salve o HMR
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        CLique nas logos do Vite e do React para saber mais!
       </p>
     </>
   )
