1a7d4180e26e5b59d3c6f03e4de1b6432ea32699
carlos.lima <gbdev11@gbringel.com>
2024-12-18 17:12:34 -0400

    Adição de uma nova sessão e footer

--- src/routes/pages/menu.tsx
+++ src/routes/pages/menu.tsx
@@ -1,9 +1,10 @@
 import { Menubar } from 'primereact/menubar';
 import { Button } from 'primereact/button';
 
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import img from '../../assets/react.svg';
+import imgDev from '../../assets/img-dev.svg';
 
 function Menu() {
 
@@ -25,7 +26,7 @@ function Menu() {
     ];
 
     const start = (
-        <img src={img} alt="Logo" style={{ width: 40, height: 40, marginRight: '10px' }} />
+        <img src={imgDev} alt="Logo" style={{ width: 40, height: 40, marginRight: '10px' }} />
     );
 
     const end = (
@@ -35,26 +36,56 @@ function Menu() {
     return (
         <div>
             <header>
-                <Menubar className='w-full bg-gray-900 border-none fixed top-0 left-0 p-3' model={items} start={start} end={end} />
+                <Menubar className='w-full bg-gray-900 border-none fixed top-0 left-0 p-3 z-1' model={items} start={start} end={end} />
             </header>
-            <main className='flex f-row px-8 mt-8 gap-8 border-solid border-1 border-green-500'>
-                <div className='align-content-center border-solid border-1 border-yellow-500'>
-                    <h1 className='text-cyan-400 text-5xl' >Bem-vindo!</h1>
-                    <p className='text-cyan-50 f-wrap mb-5'>
-                        Sou Carlos Eduardo Dias, desenvolvedor Front-End focado em criar interfaces intuitivas e funcionais. Busco constantemente aprimorar 
-                        minhas habilidades e utilizar as tecnologias mais atuais para entregar soluções de qualidade. Estou sempre aberto a
-                        aprender e a colaborar para construir experiências digitais que atendam às necessidades dos usuários.
-                    </p>
-                    <div className='flex gap-3'>
-                        <Button icon="pi pi-linkedin" rounded outlined aria-label="Filter" />
-                        <Button icon="pi pi-envelope" rounded outlined aria-label="Filter" />
-                        <Button icon="pi pi-instagram" rounded outlined aria-label="Filter" />
+            <main className='px-8 my-8 z-0'>
+                <div className='flex f-row gap-8 mb-8'>
+                    <div className='align-content-center px-8'>
+                        <h1 className='text-cyan-400 text-5xl' >Bem-vindo!</h1>
+                        <p className='text-cyan-50 f-wrap mb-5'>
+                            Sou Carlos Eduardo Dias, desenvolvedor Front-End focado em criar interfaces intuitivas e funcionais. Busco constantemente aprimorar
+                            minhas habilidades e utilizar as tecnologias mais atuais para entregar soluções de qualidade. Estou sempre aberto a
+                            aprender e a colaborar para construir experiências digitais que atendam às necessidades dos usuários.
+                        </p>
+                        <div className='flex gap-3'>
+                            <Button icon="pi pi-linkedin" rounded outlined aria-label="Filter" />
+                            <Button icon="pi pi-envelope" rounded outlined aria-label="Filter" />
+                            <Button icon="pi pi-instagram" rounded outlined aria-label="Filter" />
+                        </div>
+                    </div>
+                    <img className='' src={img} alt="logo" width={800} />
+                </div>
+                <div className='flex f-row gap-8'>
+                    <img className='' src={imgDev} alt="logo" width={400} />
+                    <div className='align-content-center px-8'>
+                        <h1 className='text-cyan-400 text-5xl' >Desenvolvimento Front-End</h1>
+                        <p className='text-cyan-50 f-wrap mb-5'>
+                            Sou Carlos Eduardo Dias, desenvolvedor Front-End focado em criar interfaces intuitivas e funcionais. Busco constantemente aprimorar
+                            minhas habilidades e utilizar as tecnologias mais atuais para entregar soluções de qualidade. Estou sempre aberto a
+                            aprender e a colaborar para construir experiências digitais que atendam às necessidades dos usuários.
+                        </p>
                     </div>
                 </div>
-                <img className='border-solid border-1 border-red-500' src={img} alt="logo" width={800} />
             </main>
-            <footer className='bg-cyan-400'>
-                <p>teste</p>
+            <footer className="bg-cyan-400 p-8">
+                <div className="p-d-flex p-ai-center">
+                    <p className="p-mr-3">© {new Date().getFullYear()} Meu Portfólio</p>
+                    <p>Desenvolvido por Carlos Eduardo Dias</p>
+                </div>
+                <div>
+                    <Button
+                        label="LinkedIn"
+                        icon="pi pi-linkedin"
+                        className="p-button-text p-mr-2 text-cyan-50"
+                        onClick={() => window.open("https://www.linkedin.com/in/carloseduardodias/", "_blank")}
+                    />
+                    <Button
+                        label="GitHub"
+                        icon="pi pi-github"
+                        className="p-button-text p-mr-2"
+                        onClick={() => window.open("https://github.com/carlosdias", "_blank")}
+                    />
+                </div>
             </footer>
         </div>
     )
