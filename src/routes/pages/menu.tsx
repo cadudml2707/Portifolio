4f86abf1ee2f3b885fbfdb6293288337c9bc0db6
carlos.lima <gbdev11@gbringel.com>
2024-12-19 15:54:00 -0400

    Componentização do Header e do Footer

--- src/routes/pages/menu.tsx
+++ src/routes/pages/menu.tsx
@@ -1,43 +1,15 @@
-import { Menubar } from 'primereact/menubar';
 import { Button } from 'primereact/button';
-
-import { useNavigate } from 'react-router-dom';
 
 import img from '../../assets/react.svg';
 import imgDev from '../../assets/img-dev.svg';
 
+import Header from '../../components/header';
+import Footer from '../../components/footer';
+
 function Menu() {
-
-    const navigate = useNavigate();
-
-    const items = [
-        {
-            label: 'Menu',
-            command: () => navigate('/')
-        },
-        {
-            label: 'Experiências',
-            command: () => navigate('/experiencias')
-        },
-        {
-            label: 'Formação',
-            command: () => navigate('/formacao')
-        }
-    ];
-
-    const start = (
-        <img src={imgDev} alt="Logo" style={{ width: 40, height: 40, marginRight: '10px' }} />
-    );
-
-    const end = (
-        <Button label='Meu Currículo' icon='pi pi-file' />
-    );
-
     return (
         <div>
-            <header>
-                <Menubar className='w-full bg-gray-900 border-none fixed top-0 left-0 p-3 z-1' model={items} start={start} end={end} />
-            </header>
+            <Header/>
             <main className='px-8 my-8 z-0'>
                 <div className='flex f-row gap-8 mb-8'>
                     <div className='align-content-center px-8'>
@@ -67,26 +39,7 @@ function Menu() {
                     </div>
                 </div>
             </main>
-            <footer className="bg-cyan-400 p-8">
-                <div className="p-d-flex p-ai-center">
-                    <p className="p-mr-3">© {new Date().getFullYear()} Meu Portfólio</p>
-                    <p>Desenvolvido por Carlos Eduardo Dias</p>
-                </div>
-                <div>
-                    <Button
-                        label="LinkedIn"
-                        icon="pi pi-linkedin"
-                        className="p-button-text p-mr-2 text-cyan-50"
-                        onClick={() => window.open("https://www.linkedin.com/in/carloseduardodias/", "_blank")}
-                    />
-                    <Button
-                        label="GitHub"
-                        icon="pi pi-github"
-                        className="p-button-text p-mr-2"
-                        onClick={() => window.open("https://github.com/carlosdias", "_blank")}
-                    />
-                </div>
-            </footer>
+            <Footer />
         </div>
     )
 }
