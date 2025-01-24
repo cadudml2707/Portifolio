851b87200b95b51dc039e27a453bec3c03e0481c
carlos.lima <gbdev11@gbringel.com>
2025-01-24 10:14:14 -0400

    Estrutura do menu Principal

--- src/routes/pages/menu.tsx
+++ src/routes/pages/menu.tsx
@@ -1,7 +1,7 @@
 import { Menubar } from 'primereact/menubar';
 import { Button } from 'primereact/button';
 
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import img from '../../assets/react.svg';
 
@@ -37,21 +37,51 @@ function Menu() {
             <header>
                 <Menubar className='w-full bg-gray-900 border-none fixed top-0 left-0 p-3' model={items} start={start} end={end} />
             </header>
-            <main className='flex f-row px-8 mt-8 gap-8 border-solid border-1 border-green-500'>
-                <div className='align-content-center border-solid border-1 border-yellow-500'>
-                    <h1 className='text-cyan-400 text-5xl' >Bem-vindo!</h1>
-                    <p className='text-cyan-50 f-wrap mb-5'>
-                        Sou Carlos Eduardo Dias, desenvolvedor Front-End focado em criar interfaces intuitivas e funcionais. Busco constantemente aprimorar 
-                        minhas habilidades e utilizar as tecnologias mais atuais para entregar soluções de qualidade. Estou sempre aberto a
-                        aprender e a colaborar para construir experiências digitais que atendam às necessidades dos usuários.
-                    </p>
-                    <div className='flex gap-3'>
-                        <Button icon="pi pi-linkedin" rounded outlined aria-label="Filter" />
-                        <Button icon="pi pi-envelope" rounded outlined aria-label="Filter" />
-                        <Button icon="pi pi-instagram" rounded outlined aria-label="Filter" />
+            <main className='px-8 mt-8 gap-8'>
+                <div className='flex flex-wrap justify-content-around'>
+                    <div className=' w-6'>
+                        <h1 className='text-cyan-400 text-5xl' >Bem-vindo!</h1>
+                        <p className='p-text-justify text-cyan-50 mb-5'>
+                            Sou Carlos Eduardo Dias, desenvolvedor Front-End focado em criar interfaces intuitivas e funcionais. Busco constantemente aprimorar
+                            minhas habilidades e utilizar as tecnologias mais atuais para entregar soluções de qualidade. Estou sempre aberto a
+                            aprender e a colaborar para construir experiências digitais que atendam às necessidades dos usuários.
+                        </p>
+                        <div className='flex gap-3'>
+                            <a href="https://www.linkedin.com/in/carlos-eduardo-dias-560935254/" target='"_blank"'>
+                                <Button icon="pi pi-linkedin" rounded outlined aria-label="Filter" />
+                            </a>
+                            <a href="mailto:carloseduardodml.profissional@gmail.com" target='"_blank"'>
+                                <Button icon="pi pi-envelope" rounded outlined aria-label="Filter" />
+                            </a>
+                            <a href="https://www.instagram.com/cadudml?igsh=MTZ3Z3MxbW5rbGN1bg==" target='"_blank"'>
+                                <Button icon="pi pi-instagram" rounded outlined aria-label="Filter" />
+                            </a>
+                        </div>
+                    </div>
+                    <img className='' src={img} alt="logo" width={400} />
+                </div>
+                <div className='flex flex-wrap justify-content-around mt-8'>
+                    <img className='' src={img} alt="logo" width={400} />
+                    <div className=' w-6'>
+                        <h1 className='text-cyan-400 text-5xl' >Desenvolvimento</h1>
+                        <p className='p-text-justify text-cyan-50 mb-5'>
+                            Sou Carlos Eduardo Dias, desenvolvedor Front-End focado em criar interfaces intuitivas e funcionais. Busco constantemente aprimorar
+                            minhas habilidades e utilizar as tecnologias mais atuais para entregar soluções de qualidade. Estou sempre aberto a
+                            aprender e a colaborar para construir experiências digitais que atendam às necessidades dos usuários.
+                        </p>
+                        <div className='flex gap-3'>
+                            <a href="https://www.linkedin.com/in/carlos-eduardo-dias-560935254/" target='"_blank"'>
+                                <Button icon="pi pi-linkedin" rounded outlined aria-label="Filter" />
+                            </a>
+                            <a href="mailto:carloseduardodml.profissional@gmail.com" target='"_blank"'>
+                                <Button icon="pi pi-envelope" rounded outlined aria-label="Filter" />
+                            </a>
+                            <a href="https://www.instagram.com/cadudml?igsh=MTZ3Z3MxbW5rbGN1bg==" target='"_blank"'>
+                                <Button icon="pi pi-instagram" rounded outlined aria-label="Filter" />
+                            </a>
+                        </div>
                     </div>
                 </div>
-                <img className='border-solid border-1 border-red-500' src={img} alt="logo" width={800} />
             </main>
             <footer className='bg-cyan-400'>
                 <p>teste</p>
