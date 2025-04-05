3b9a3d9150995a4fe47be3af0865df9608297acb
carlos.lima <carloseduardodml@gmail.com>
2025-04-05 14:45:15 -0400

    Criação da branch de Formações

--- src/routes/pages/formacao.tsx
+++ src/routes/pages/formacao.tsx
@@ -10,7 +10,7 @@ function Formacao() {
         <div>
             <Header/>
             <main className='flex f-row px-8 mt-8 gap-8'>
-                <h1 className='text-cyan-50'>FORMAÇÃO</h1>
+                <h1 className='text-cyan-50'>TESTE TESTE TESTE</h1>
             </main>
             <Footer/>
         </div>
