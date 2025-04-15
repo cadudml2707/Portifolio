dc882274ab3c2ccb8f483e7d61b46c7b66637e83
carlos.lima <gbdev11@gbringel.com>
2025-04-15 13:34:13 -0400

    Estilização do footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,22 +3,23 @@ import { Button } from 'primereact/button';
 function Footer() {
     return (
         <footer>
-            <div className="flex justify-content-center p-8 bg-cyan-600">
-                <Button
-                    label="LinkedIn"
-                    icon="pi pi-linkedin"
-                    className="p-button-text p-mr-2 text-cyan-50"
-                    onClick={() => window.open("https://www.linkedin.com/in/carlos-eduardo-dias-560935254/", "_blank")}
-                />
-                <Button
-                    label="GitHub"
-                    icon="pi pi-github"
-                    className="p-button-text p-mr-2"
-                    onClick={() => window.open("https://github.com/cadudml2707", "_blank")}
-                />
+            <div className="lg:flex sm:flex-row justify-content-center gap-8 p-8 bg-cyan-600">
+                <div className="flex-row flex-wrap justify-content-start gap-3 text-white">
+                    <h3 className='border-bottom-2 border-orange-500 pb-3 white-space-nowrap' style={{ width: '20%' }}>Sobre mim</h3>
+                    <p>Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum</p>
+                </div>
+                <div className="flex-row justify-content-start gap-3 text-white">
+                    <h3 className='border-bottom-2 border-orange-500 pb-3' style={{ width: '20%' }}>
+                        Contatos
+                    </h3>
+                    <p>linkedin</p>
+                    <p>github</p>
+                    <p>carloseduardodml.profissional@gmail.com</p>
+                    <p>instagram</p>
+                </div>
             </div>
-            <div className="flex justify-content-center p-1 bg-gray-600 text-cyan-50">
-                <p className="p-mr-3">© {new Date().getFullYear()} Meu Portfólio </p> 
+            <div className="flex justify-content-center p-1 bg-gray-700 text-cyan-50">
+                <p className="p-mr-3">© {new Date().getFullYear()} Carlos Eduardo Dias </p>
             </div>
         </footer>
     );
