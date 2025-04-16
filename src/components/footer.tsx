c51bda4d6f3ca41a18d6dd92d4defcabe1efac8a
carlos.lima <gbdev11@gbringel.com>
2025-04-16 11:53:53 -0400

    Finalização do footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,32 +4,34 @@ function Footer() {
     return (
         <footer>
             <div className="lg:flex sm:flex-row justify-content-center gap-8 p-8 bg-cyan-600">
-                <div className="flex-row flex-wrap justify-content-start gap-3 text-white">
-                    <h3 className='border-bottom-2 border-orange-500 pb-3 white-space-nowrap' style={{ width: '20%' }}>Sobre mim</h3>
-                    <p>Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum</p>
+                <div className="flex-row flex-wrap justify-content-start gap-3 lg:w-3 md:w-5 sm:w-5 text-white">
+                    <h3 className='border-bottom-2 border-orange-500 pb-3 w-1 white-space-nowrap'>Sobre mim</h3>
+                    <p className='text-justify'>Desenvolvedor Full Stack com foco em ReactJS, TypeScript, Java e UX/UI Design. Formado em Sistemas de Informação, 
+                        trabalho criando interfaces intuitivas e tenho afinidade com POO. Busco consolidar minha carreira como dev full-stack, 
+                        aprofundando conhecimentos em back-end para entregar soluções completas.</p>
                 </div>
-                <div className="flex-row justify-content-start gap-3 text-white">
-                    <h3 className='border-bottom-2 border-orange-500 pb-3' style={{ width: '20%' }}>
+                <div className="flex-row justify-content-start gap-3 lg:w-3 sm:w-5 text-white">
+                    <h3 className='border-bottom-2 border-orange-500 pb-3 w-1'>
                         Contatos
                     </h3>
                     <p>
-                        <a className='no' href="https://www.linkedin.com/in/carlos-eduardo-dias-560935254/" target='_blanck'>
-                            linkedin
+                        <a className='no-underline' href="https://www.linkedin.com/in/carlos-eduardo-dias-560935254/" target='_blanck'>
+                            <Button icon="pi pi-linkedin" className='bg-cyan-600 border-none gap-2'>Linkedin</Button>
                         </a>
                     </p>
                     <p>
-                        <a href="https://github.com/cadudml2707">
-                            github
+                        <a className='no-underline' href="https://github.com/cadudml2707">
+                            <Button icon="pi pi-github" className='bg-cyan-600 border-none gap-2'>GitHub</Button>
                         </a>
                     </p>
                     <p>
-                        <a href="mailto:carloseduardodml.profissional@gmail.com">
-                            carloseduardodml.profissional@gmail.com
+                        <a className='no-underline' href="mailto:carloseduardodml.profissional@gmail.com">
+                            <Button icon="pi pi-envelope" className='bg-cyan-600 border-none gap-2'>Email</Button>
                         </a>
                     </p>
                     <p>
-                        <a href="https://www.instagram.com/cadudml?igsh=MTZ3Z3MxbW5rbGN1bg==">
-                            instagram
+                        <a className='no-underline' href="https://www.instagram.com/cadudml?igsh=MTZ3Z3MxbW5rbGN1bg==">
+                            <Button icon="pi pi-instagram" className='bg-cyan-600 border-none gap-2'>Instagram</Button>
                         </a>
                     </p>
                 </div>
